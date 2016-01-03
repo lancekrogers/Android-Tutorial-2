1017d864147ff06ebbcc55b7d9b9b0417186ff9b
Lance Rogers <lancekincaid1994@gmail.com>
2016-01-03 16:06:59 -0500

    working on the story activity

--- app/src/main/java/com/lancekrogers/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/lancekrogers/interactivestory/ui/StoryActivity.java
@@ -1,16 +1,35 @@
 package com.lancekrogers.interactivestory.ui;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.lancekrogers.interactivestory.R;
+import com.lancekrogers.interactivestory.model.Page;
+import com.lancekrogers.interactivestory.model.Story;
 
 
 public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story mStory = new Story();
+
+    private ImageView mImageView;
+
+    private TextView mTextView;
+
+    private Button mChoice1;
+
+    private Button mChoice2;
 
 
     @Override
@@ -28,8 +47,22 @@ public class StoryActivity extends AppCompatActivity {
         Log.d(TAG, name);
 
 
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceButton1);
+        mChoice2 = (Button)findViewById(R.id.choiceButton2);
+    }
 
+    private void loadPage() {
+        Page page = mStory.getPage(0);
 
+        Drawable drawable = ContextCompat.getDrawable(null, page.getImageId());
+        mImageView.setImageDrawable(drawable);
+
+        mTextView.setText(page.getText());
+
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
     }
 
 }
