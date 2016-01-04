cf08be8cdabe7b8ea442c04f0017e86339722b49
Lance Rogers <lancekincaid1994@gmail.com>
2016-01-04 18:00:38 -0500

    Late for work commit

--- app/src/main/java/com/lancekrogers/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/lancekrogers/interactivestory/ui/StoryActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -35,6 +36,8 @@ public class StoryActivity extends AppCompatActivity {
 
     private String mName;
 
+    private Page mCurrentPage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,22 +58,38 @@ public class StoryActivity extends AppCompatActivity {
         mTextView = (TextView)findViewById(R.id.storyTextView);
         mChoice1 = (Button)findViewById(R.id.choiceButton1);
         mChoice2 = (Button)findViewById(R.id.choiceButton2);
-        loadPage();
+        loadPage(0);
     }
 
-    private void loadPage() {
-        Random randInt = new Random();
-        Page page = mStory.getPage(randInt.nextInt(6));
-        int imageId = page.getImageId();
+    private void loadPage(int Choice) {
+        mCurrentPage = mStory.getPage(Choice);
+        int imageId = mCurrentPage.getImageId();
         Drawable drawable = ContextCompat.getDrawable(this, imageId);
         mImageView.setImageDrawable(drawable);
         // Add the users name to the story
-        String pageText = page.getText();
+        String pageText = mCurrentPage.getText();
         pageText = String.format(pageText, mName);
         mTextView.setText(pageText);
 
-        mChoice1.setText(page.getChoice1().getText());
-        mChoice2.setText(page.getChoice2().getText());
+        mChoice1.setText(mCurrentPage.getChoice1().getText());
+        mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+        mChoice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               int nextPage =  mCurrentPage.getChoice1().getNextPage();
+               loadPage(nextPage);
+
+            }
+        });
+        mChoice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nextPage = mCurrentPage.getChoice2().getNextPage();
+                loadPage(nextPage);
+
+            }
+        });
     }
 
 }
