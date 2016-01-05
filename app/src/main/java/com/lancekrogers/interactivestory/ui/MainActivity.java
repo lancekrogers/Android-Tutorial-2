332c463c1238b752caa6814e823fcb4a32e18f26
Lance Rogers <lancekincaid1994@gmail.com>
2016-01-05 17:57:54 -0500

    clear namefield onResume

--- app/src/main/java/com/lancekrogers/interactivestory/ui/MainActivity.java
+++ app/src/main/java/com/lancekrogers/interactivestory/ui/MainActivity.java
@@ -41,5 +41,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mNameField.setText("");
+    }
 }
